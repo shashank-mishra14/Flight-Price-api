cf0e8e42dd160787027702d390a7f055450736dd
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-05-08 21:27:58 +0530

    Changes

--- index.js
+++ index.js
@@ -4,28 +4,34 @@ const cheerio=require('cheerio')
 const axios=require('axios')
 
 const app = express()
-
-const results =[
-
-]
-
-const flightprice=[
-    {
-        "flight":"Air India",
-        "address":"https://www.airindia.in/"
-    },
-    {
-        "flight":"Indigo",
-        "address":"https://www.goindigo.in/"
-    },
-    {
-        "flight":"Spicejet",
-        "address":"https://www.spicejet.com/"
-    },
-]
-
+const results = []
 app.get('/', (req, res) => {                            //get request from home pageN
-    res.json("Welcom to Flight Price API")
+    res.json("Welcome to Flight Price API")
+})
+
+app.get('/flights', (req, res) => {                       //get request from /flight page
+    axios.get('https://www.expedia.com/Flights')
+    .then((response) => {
+      const html=response.data
+        const $ = cheerio.load(html)
+
+        $('div.uitk-layout-grid-item',html).each(function() {
+
+            const title = $(this).find('h3').text()
+            const price = $(this).find('span.uitk-lockup-price').text()
+            const link = $(this).find('a').attr('href')
+            const image = $(this).find('img').attr('src')
+            results.push({
+                title,
+                price,
+                link,
+                image
+            })
+        }
+        )
+        res.json(results)
+    })
+
 })
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
