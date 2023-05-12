4b1660417bcdb6a546596ce05693d87541e2051c
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-05-12 23:28:18 +0530

    changes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {                            //get request from home 
 })
 
 app.get('/flights', (req, res) => {                       //get request from /flight page
-    axios.get('https://www.expedia.com/Flights')
+    axios.get('https://www.expedia.com/Flights-Search?leg1=from%3ADelhi%20%28DEL%20-%20Indira%20Gandhi%20Intl.%29%2Cto%3AMumbai%20%28BOM%20-%20Chhatrapati%20Shivaji%20Intl.%29%2Cdeparture%3A5%2F22%2F2023TANYT&leg2=from%3AMumbai%20%28BOM%20-%20Chhatrapati%20Shivaji%20Intl.%29%2Cto%3ADelhi%20%28DEL%20-%20Indira%20Gandhi%20Intl.%29%2Cdeparture%3A5%2F23%2F2023TANYT&mode=search&options=carrier%3A%2A%2Ccabinclass%3A%2Cmaxhops%3A1%2Cnopenalty%3AN&pageId=0&passengers=adults%3A1%2Cchildren%3A0%2Cinfantinlap%3AN&trip=roundtrip')
     .then((response) => {
       const html=response.data
         const $ = cheerio.load(html)
@@ -18,14 +18,10 @@ app.get('/flights', (req, res) => {                       //get request from /fl
         $('div.uitk-layout-grid-item',html).each(function() {
 
             const title = $(this).find('h3').text()
-            const price = $(this).find('span.uitk-lockup-price').text()
-            const link = $(this).find('a').attr('href')
-            const image = $(this).find('img').attr('src')
+            const price = $(this).find('span.full-bold.no-wrap').text()
             results.push({
                 title,
-                price,
-                link,
-                image
+                price
             })
         }
         )
